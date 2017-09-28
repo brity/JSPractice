a8548aaeda7c74bf87352bd5479c9f5270f28a75
brity <qisipu1997@gmail.com>
2017-09-28 18:46:42 -0400

    first commit

---
+++ if-statement.js
@@ -0,0 +1,8 @@
+var fruit = 'orange'
+var result;
+if (fruit.length > 5){
+  result = 'The fruit name has more than five characters.'
+}else{
+  result = 'The fruit name has five characters or less.'
+}
+console.log(result);
